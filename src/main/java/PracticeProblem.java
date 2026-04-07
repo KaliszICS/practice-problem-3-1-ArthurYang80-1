d50f7906b93f784708f12fda40771e706ca03fcf
ArthurYang80 <342802865@tdsb.ca>
2026-04-07 21:19:45 +0000

    Finalized

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,28 @@ public class PracticeProblem {
 
 	}
 
+	public static int sumOfDigits(int x) {
+		if (x < 0) {
+			x = x * -1;
+		}
+		
+		if (x >= 0 && x <= 9) {
+			return x;
+		}
+		
+		return (x % 10) + sumOfDigits(x / 10);
+	}
 	
-
+	public static int factorial(int n) {
+		if (n < 0) {
+			return 0;
+		}
+		
+		if (n == 0) {
+			return 1;
+		}
+		
+		return n * factorial(n - 1);
+	}
 }
+
